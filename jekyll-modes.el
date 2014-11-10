81dad2a0bca20cd609e475fdc5bdf1d324ef96b2
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2014-11-10 22:11:34 +0100

    working Markdown and HTML modes (no code snippet support)

--- jekyll-modes.el
+++ jekyll-modes.el
@@ -4,8 +4,8 @@
 (defcustom jekyll/yaml-frontmatter
   (pm-hbtchunkmode "yaml"
                    :mode 'yaml-mode
-                   :head-reg "\\`---"
-                   :tail-reg "[^\\`]---")
+                   :head-reg "\\`---\n"
+                   :tail-reg "[^\\`]---\n")
   "Yaml chunk"
   :group 'innermodes
   :type 'object)
@@ -28,11 +28,27 @@
   :group 'innermodes
   :type 'object)
 
+;; (defcustom jekyll/pygments
+;;   (pm-hbtchunkmode-auto "markdown"
+;;                         :head-reg "{%\s*highlight\s*\\(.*\\)\s%}"
+;;                         :tail-reg "{%\s*endhighlight\s*%}"
+;;                         :head-mode 'liquid-tags-mode
+;;                         :tail-mode 'liquid-tags-mode
+;;                         :retriever-regexp "{%\s*highlight\s*\\(.*\\)\s%}"
+;;                         :font-lock-narrow t)
+;;   "Pygments chunk"
+;;   :group 'innermodes
+;;   :type 'object)
+
 (defcustom jekyll/markdown 
   (pm-polymode-multi "markdown"
                      :hostmode 'pm-host/markdown
                      :innermodes '(jekyll/yaml-frontmatter
-                                   jekyll/liquid-tag jekyll/liquid-expression))
+                                   pm-inner/markdown
+                                   ;; jekyll/pygments
+                                   jekyll/liquid-expression
+                                   jekyll/liquid-tag
+                                   ))
   "Markdown with YAML frontmatter and Liquid tags support."
   :group 'polymodes
   :type 'object)
@@ -47,7 +63,10 @@
   :group 'polymodes
   :type 'object)
 
+;;;###autoload (autoload 'jekyll-markdown-mode "jekyll-modes")
 (define-polymode jekyll-markdown-mode jekyll/markdown)
+
+;;;###autoload (autoload 'jekyll-html-mode "jekyll-modes")
 (define-polymode jekyll-html-mode jekyll/html)
 
 (provide 'jekyll-modes)
